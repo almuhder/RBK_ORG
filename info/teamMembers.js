fff1d9cd564fc761a932e69472473d801c1d7305
Besslan <besslan.bazadough@gmail.com>
2018-12-18 17:20:57 +0200

    add new pictures for staff, change staff desc

--- info/teamMembers.js
+++ info/teamMembers.js
@@ -1,99 +1,99 @@
 var staff = [
   {
     "name": "Ahmad", 
-    "occupation": "Class Coordinator",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, repellat, eos! Dignissimos expedita totam cum quidem autem alias dolorum error a quia optio eligendi, amet animi reprehenderit, quaerat.",
+    "occupation": "Student Experience",
+    "description": "Easy going, humble, open minded and social person who loves fitness. Ahmad aims to change at least a small part of his community to become more educated and understanding of how the world works",
     "image": "img/team/Ahmed.jpg"
   },
   {
     "name": "Balqees", 
-    "occupation": "Admission",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, repellat, eos! Dignissimos expedita totam cum quidem autem alias dolorum error a quia optio eligendi, amet animi reprehenderit, quaerat.",
+    "occupation": "Admissions Coordinator",
+    "description": "The quiet one. Balqees brings the laughs to the Admissions environment. Always helpful, she guides applicants through their Admissions journey and arms them with the tools they need to successfully be admitted to RBK’s Immersive Program.",
     "image": "img/team/Balqees.jpg"
   },
   {
     "name": "Bayan", 
-    "occupation": "Admission",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, repellat, eos! Dignissimos expedita totam cum quidem autem alias dolorum error a quia optio eligendi, amet animi reprehenderit, quaerat.",
+    "occupation": "Admissions Manager",
+    "description": "Knowledgeable but funny and charismatic, Bayan will never give in to a bad day. A day is what you make of it, and even though it looks like it’s raining, she has a talent of making the sun shine around her all the time.",
     "image": "img/team/Bayan.jpg"
   },
   {
     "name": "Besslan", 
-    "occupation": "HIR",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, repellat, eos! Dignissimos expedita totam cum quidem autem alias dolorum error a quia optio eligendi, amet animi reprehenderit, quaerat.",
+    "occupation": "Curriculum Facilitator",
+    "description": "Besslan is a realistic logical person who enjoys solving problems, even though he looks serious all the time he actually likes having fun and making others laugh with his weird sense of humor",
     "image": "img/team/Besslan.jpg"
   },
   {
     "name": "Esraa", 
-    "occupation": "HIR",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, repellat, eos! Dignissimos expedita totam cum quidem autem alias dolorum error a quia optio eligendi, amet animi reprehenderit, quaerat.",
+    "occupation": "Outcomes Technician",
+    "description": "As software engineer she enjoys the gift of life. She believes in balance. Whenever you try your best to improve and share happiness with others, you will always be surrounded by beauty and achievements",
     "image": "img/team/Esraa.jpg"
   },
   {
     "name": "Kelsey", 
     "occupation": "Counselor",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, repellat, eos! Dignissimos expedita totam cum quidem autem alias dolorum error a quia optio eligendi, amet animi reprehenderit, quaerat.",
+    "description": "She loves talking to people about what makes them unique and how they can offer that to the world. In her free time, she enjoys exploring ethnic foods, outdoor adventures, and people watching in cafes ",
     "image": "img/team/Kelsey.jpg"
   },
   {
     "name": "Mohammed", 
-    "occupation": "HIR",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, repellat, eos! Dignissimos expedita totam cum quidem autem alias dolorum error a quia optio eligendi, amet animi reprehenderit, quaerat.",
+    "occupation": "Curriculum Facilitator",
+    "description": "Jozaa is crazy, he is always spreading positive energy, fun jokes, and likes to make others smile. He admires algorithms and has awesome ways to explain anything to make it easier to understand",
     "image": "img/team/Jozaa.jpg"
   },
   {
     "name": "Sahar", 
-    "occupation": "Technical Monitor",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, repellat, eos! Dignissimos expedita totam cum quidem autem alias dolorum error a quia optio eligendi, amet animi reprehenderit, quaerat.",
+    "occupation": "Tech Mentor",
+    "description": "She looks on this app called Stack Overflow for at least 3 hours a day and doesn’t get bored. Her hobby is playing with her cat. She loves 2 things, and that is sleeping and coding",
     "image": "img/team/Sahar.jpg"
   },
   {
     "name": "Tal", 
-    "occupation": "HIR",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, repellat, eos! Dignissimos expedita totam cum quidem autem alias dolorum error a quia optio eligendi, amet animi reprehenderit, quaerat.",
+    "occupation": "Curriculum Facilitator",
+    "description": "Just like a book, don't judge her by her cover. She’s funny from the inside and an RBK graduate that helps the students find their way during the program. She’s super open to people and always makes sure they have a smile on their face.",
     "image": "img/team/Tal.jpg"
   },
   {
     "name": "Zainab", 
-    "occupation": "HIR",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, repellat, eos! Dignissimos expedita totam cum quidem autem alias dolorum error a quia optio eligendi, amet animi reprehenderit, quaerat.",
+    "occupation": "Admission Coordinator",
+    "description": "A bundle of joy, warmth and professionalism. Known for going the extra mile to help others and offer support. If you’re feeling down and out, take two Zainabs with a full glass of water and call us in the morning.",
     "image": "img/team/Zainab.jpg"
   },
 
   {
     "name": "Fatima", 
     "occupation": "Program Manager",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, repellat, eos! Dignissimos expedita totam cum quidem autem alias dolorum error a quia optio eligendi, amet animi reprehenderit, quaerat.",
+    "description": "Fatema is a graduate from cohort 1 and wants to change peoples’ lives. After three years of unemployment, she never thought that one day she will be running one of RBK's cohorts. Fatema encourages all females to learn coding and always says “never ever stop learning”   ",
     "image": "img/team/fatima.jpg"
   },
   {
     "name": "Hannan", 
-    "occupation": "Outcomes",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, repellat, eos! Dignissimos expedita totam cum quidem autem alias dolorum error a quia optio eligendi, amet animi reprehenderit, quaerat.",
+    "occupation": "Outcomes Technician",
+    "description": "Always top of her class, she started with JavaScript and ended up with Blockchain ! Now she helps our students to fine tune their skills to get their dream job. More importantly, the best Arabic coffee maker in Jordan !",
     "image": "img/team/hannan.jpg"
   },
   {
     "name": "Sanaa", 
-    "occupation": "Chef",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, repellat, eos! Dignissimos expedita totam cum quidem autem alias dolorum error a quia optio eligendi, amet animi reprehenderit, quaerat.",
+    "occupation": "Executive Chef",
+    "description": "Always smiling but always busy making sure everyone gets their daily nutrition in support of their work. Sana makes sure that everyone around her is fed with happiness. If you like great food, it’s guaranteed you’ll be happy !",
     "image": "img/team/Sanaa.jpg"
   },
   {
     "name": "Sarah", 
-    "occupation": "Outcomes",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, repellat, eos! Dignissimos expedita totam cum quidem autem alias dolorum error a quia optio eligendi, amet animi reprehenderit, quaerat.",
+    "occupation": "Outcomes Coordinator",
+    "description": "She is in love with her career. “I love how I can see my job changing people's lives”. A creative thinker, she is the main link between our graduates and the professional tech world. She never gives up, and will never let you give up !",
     "image": "img/team/Sarah.jpg"
   },
   {
     "name": "ocean", 
-    "occupation": "Outcomes",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, repellat, eos! Dignissimos expedita totam cum quidem autem alias dolorum error a quia optio eligendi, amet animi reprehenderit, quaerat.",
+    "occupation": "Curriculum Facilitator",
+    "description": "With a Computer Vision and Engineering background, he has experience ranging from low-level Assembly Language to web-app. For him, software is about finding the balance between creativity and utility. At RBK, he is striving to deliver complex C.S. concepts creatively to our students.",
     "image": "img/team/ocean.jpg"
   },
   {
     "name": "Elyse", 
-    "occupation": "Outcomes",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, repellat, eos! Dignissimos expedita totam cum quidem autem alias dolorum error a quia optio eligendi, amet animi reprehenderit, quaerat.",
+    "occupation": "Curriculum Facilitator",
+    "description": "Former professional musician, educator and a graduate of Hack Reactor. An ability to bring creative solutions to teaching complex technical concepts to the students at RBK. In her free time, Elyse enjoys reading, running, lifting weights, hiking, and playing her saxophone. ",
     "image": "img/team/Elyse.jpg"
   }
 ]
